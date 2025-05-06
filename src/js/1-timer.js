f511361b4d9d3e0bee2312751ce187264f33fa87
georgiy <heorhii.kyryliuk@gmail.com>
2025-05-06 21:18:33 +0300

    timer-done

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -19,25 +19,35 @@ flatpickr('#datetime-picker', options);
 
 const refs = {
   dateInputEl: document.getElementById('datetime-picker'),
-  startBtn: document.querySelector('.js-flatpickr button'),
   timerEl: document.querySelector('.timer'),
+  startBtn: document.querySelector('[data-start]'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
 };
 
-refs.startBtn.addEventListener('click', handleStartBtnClick);
+refs.startBtn.addEventListener('click', onStartBtnClick);
 
 function isFutureDate(date) {
   return date instanceof Date && date.getTime() > Date.now();
 }
 
+function timeDiff(date) {
+  return date.getTime() - Date.now();
+}
+
 function enableTimer() {
   refs.startBtn.classList.add('active');
+  refs.startBtn.disabled = false;
 }
 
 function disableTimer() {
   refs.startBtn.classList.remove('active');
+  refs.startBtn.disabled = true;
 }
 
-function showMessage() {
+function showErrorMessage() {
   iziToast.error({
     title: 'Error',
     message: 'Please choose a date in the future',
@@ -45,12 +55,21 @@ function showMessage() {
   });
 }
 
+function showSuccesMessage() {
+  iziToast.success({
+    title: 'Done!',
+    message: 'Time is up! Now click Start button.',
+    position: 'topRight',
+  });
+}
+
 function dealWithTimer() {
   if (isFutureDate(userDate)) {
     enableTimer();
+    showSuccesMessage();
   } else {
     disableTimer();
-    showMessage();
+    showErrorMessage();
   }
 }
 
@@ -62,39 +81,77 @@ function enableCalendar() {
   refs.dateInputEl.disabled = false;
 }
 
-function handleStartBtnClick(event) {
-  blockCalendar();
+function doubleNumToString(number) {
+  return String(number).padStart(2, '0');
 }
 
-console.log(refs.timerEl.children);
+function onStartBtnClick() {
+  TIMER.run();
+}
+
 const TIMER = {
   days: null,
   hours: null,
   minutes: null,
   seconds: null,
+  intervalId: null,
+
+  run() {
+    blockCalendar();
+    disableTimer();
+
+    this.update();
+
+    this.intervalId = setInterval(() => {
+      if (timeDiff(userDate) <= 0) {
+        clearInterval(this.intervalId);
+        this.reset();
+        enableCalendar();
+        return;
+      }
+
+      this.update();
+    }, 1000);
+  },
+
+  update() {
+    const { days, hours, minutes, seconds } = convertMs(timeDiff(userDate));
+
+    this.days = doubleNumToString(days);
+    this.hours = doubleNumToString(hours);
+    this.minutes = doubleNumToString(minutes);
+    this.seconds = doubleNumToString(seconds);
+
+    this.render();
+  },
+
+  render() {
+    const { daysEl, hoursEl, minutesEl, secondsEl } = refs;
+
+    daysEl.textContent = this.days;
+    hoursEl.textContent = this.hours;
+    minutesEl.textContent = this.minutes;
+    secondsEl.textContent = this.seconds;
+  },
+
+  reset() {
+    this.days = null;
+    this.hours = null;
+    this.minutes = null;
+    this.seconds = null;
+    this.intervalId = null;
+  },
 };
 
-const childs = refs.timerEl.children;
-for (const child of childs) {
-  console.log(child.children);
-  const [value, label] = child.children;
-  console.log(value, label);
-}
-
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
