07aa17cd734011a12b7eb8e84c6eeb0f9f0aaa66
georgiy <heorhii.kyryliuk@gmail.com>
2025-05-06 18:12:56 +0300

    small-upt-add-couple-func

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,7 +3,7 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-let userSelectedDate = null;
+let userDate = null;
 
 const options = {
   enableTime: true,
@@ -11,11 +11,75 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    userDate = selectedDates[0];
+    dealWithTimer();
   },
 };
-
 flatpickr('#datetime-picker', options);
+
+const refs = {
+  dateInputEl: document.getElementById('datetime-picker'),
+  startBtn: document.querySelector('.js-flatpickr button'),
+  timerEl: document.querySelector('.timer'),
+};
+
+refs.startBtn.addEventListener('click', handleStartBtnClick);
+
+function isFutureDate(date) {
+  return date instanceof Date && date.getTime() > Date.now();
+}
+
+function enableTimer() {
+  refs.startBtn.classList.add('active');
+}
+
+function disableTimer() {
+  refs.startBtn.classList.remove('active');
+}
+
+function showMessage() {
+  iziToast.error({
+    title: 'Error',
+    message: 'Please choose a date in the future',
+    position: 'topRight',
+  });
+}
+
+function dealWithTimer() {
+  if (isFutureDate(userDate)) {
+    enableTimer();
+  } else {
+    disableTimer();
+    showMessage();
+  }
+}
+
+function blockCalendar() {
+  refs.dateInputEl.disabled = true;
+}
+
+function enableCalendar() {
+  refs.dateInputEl.disabled = false;
+}
+
+function handleStartBtnClick(event) {
+  blockCalendar();
+}
+
+console.log(refs.timerEl.children);
+const TIMER = {
+  days: null,
+  hours: null,
+  minutes: null,
+  seconds: null,
+};
+
+const childs = refs.timerEl.children;
+for (const child of childs) {
+  console.log(child.children);
+  const [value, label] = child.children;
+  console.log(value, label);
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -35,7 +99,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
